d819b5065e9b6681f46013ce48dc638afdb30a4a
naninunenoy <naninuneno0411@gmail.com>
2022-03-24 18:19:02 +0900

    BaseValue

--- ijs/BaseValue.cs
+++ ijs/BaseValue.cs
@@ -1,3 +1,17 @@
-﻿namespace ijs;
+﻿using System.Globalization;
 
-public class BaseValue { }
+namespace ijs;
+
+/// <summary>
+/// 基礎点
+/// </summary>
+public readonly struct BaseValue {
+    public readonly float value;
+    public BaseValue(float value) {
+        this.value = value;
+    }
+
+    public override string ToString() {
+        return $"value={value.ToString(CultureInfo.InvariantCulture)}";
+    }
+}
--- ijs_test/BaseValueTest.cs
+++ ijs_test/BaseValueTest.cs
@@ -5,7 +5,8 @@ namespace ijs_test;
 
 public class BaseValueTest {
     [Fact]
-    public void Test1() {
-        var x = new BaseValue();
+    public void ConstructorTest() {
+        var x = new BaseValue(11);
+        Assert.Equal("value=11", x.ToString());
     }
 }
