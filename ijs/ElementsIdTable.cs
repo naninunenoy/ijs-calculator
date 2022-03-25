120866808f44cb6e62161e6c099cd004a23b31f2
naninunenoy <naninuneno0411@gmail.com>
2022-03-25 19:48:56 +0900

    IDテーブルのテスト

--- ijs/ElementsIdTable.cs
+++ ijs/ElementsIdTable.cs
@@ -25,17 +25,18 @@ public static class ElementsIdTable {
     public static IEnumerable<ElementsId> SingleSpinElementsIds() {
         return AllCode
             .Where(code => code is >= ElementsCode.USp and <= ElementsCode.FCCoSp)
-            .Select(code => (code, allLevel: AllLevel))
+            .Select(code => (code, AllLevel))
             .SelectMany(ToLevelElementIds);
     }
 
     public static IEnumerable<ElementsId> SingleStepSequenceElementsIds() {
         return AllCode
-            .Where(code => code is >= ElementsCode.StSq and <= ElementsCode.ChSq)
-            .Select(code => (code, allLevel: AllLevel))
-            .SelectMany(ToLevelElementIds);
+            .Where(code => code is ElementsCode.StSq)
+            .Select(code => (code, AllLevel))
+            .SelectMany(ToLevelElementIds)
+            .Concat(new[] { new ElementsId(ElementsCode.ChSq) });
     }
-    
+
     public static IEnumerable<ElementsId> PairJumpElementsIds() {
         return SingleJumpElementsIds();
     }
@@ -47,41 +48,41 @@ public static class ElementsIdTable {
     public static IEnumerable<ElementsId> PairLiftElementsIds() {
         return AllCode
             .Where(code => code is >= ElementsCode._1Li and <= ElementsCode._5RLi)
-            .Select(code => (code, allLevel: AllLevel))
+            .Select(code => (code, AllLevel))
             .SelectMany(ToLevelElementIds);
     }
     
     public static IEnumerable<ElementsId> PairTwistLiftElementsIds() {
         return AllCode
             .Where(code => code is >= ElementsCode._1Tw and <= ElementsCode._4Tw)
-            .Select(code => (code, allLevel: AllLevel))
+            .Select(code => (code, AllLevel))
             .SelectMany(ToLevelElementIds);
     }
-    
+
     public static IEnumerable<ElementsId> PairThrowJumpElementsIds() {
         return AllCode
-            .Where(code => code is >= ElementsCode._1Tw and <= ElementsCode._4Tw)
-            .Select(code => (code, allLevel: AllLevel))
-            .SelectMany(ToLevelElementIds);
+            .Where(code => code is >= ElementsCode._1TTh and <= ElementsCode._4LzTh)
+            .Select(code => new ElementsId(code));
     }
-    
+
     public static IEnumerable<ElementsId> PairDeathSpiralElementsIds() {
         return AllCode
-            .Where(code => code is >= ElementsCode.FiDs and <= ElementsCode.PiF)
-            .Select(code => (code, allLevel: AllLevel))
-            .SelectMany(ToLevelElementIds);
+            .Where(code => code is >= ElementsCode.FiDs and <= ElementsCode.BoDs)
+            .Select(code => (code, AllLevel))
+            .SelectMany(ToLevelElementIds)
+            .Concat(new[] { new ElementsId(ElementsCode.PiF) });
     }
-    
+
     public static IEnumerable<ElementsId> PairSpinElementsIds() {
         return AllCode
             .Where(code => code is >= ElementsCode.PSp and <= ElementsCode.PCoSp)
-            .Select(code => (code, allLevel: AllLevel))
+            .Select(code => (code, AllLevel))
             .SelectMany(ToLevelElementIds);
     }
 
     public static IEnumerable<ElementsId> IceDancePatternDanceElementsIds() {
         return AllCode
-            .Where(code => code is >= ElementsCode.PSp and <= ElementsCode.PCoSp)
+            .Where(code => code is >= ElementsCode._1BL and <= ElementsCode._1MN)
             .Select(code => (code, AllLevel.Where(lv => lv is not ElementsLevel.LvB)))
             .SelectMany(ToLevelElementIds);
     }
@@ -110,7 +111,7 @@ public static class ElementsIdTable {
     public static IEnumerable<ElementsId> IceDanceStepSequenceElementsIds() {
         return AllCode
             .Where(code => code is >= ElementsCode.PSt and <= ElementsCode.NtCiSt)
-            .Select(code => (code, allLevel: AllLevel))
+            .Select(code => (code, AllLevel))
             .SelectMany(ToLevelElementIds);
     }
 
---
+++ ijs_test/ElementsIdTableTest.cs
@@ -0,0 +1,103 @@
+﻿using System.Linq;
+using ijs;
+using Xunit;
+
+namespace ijs_test;
+
+public class ElementsIdTableTest {
+    [Fact]
+    public void SingleJumpElementsIdsTest() {
+        var ids = ElementsIdTable.SingleJumpElementsIds();
+        Assert.Equal(25, ids.Count());
+    }
+
+    [Fact]
+    public void SingleSpinElementsIdsTest() {
+        var ids = ElementsIdTable.SingleSpinElementsIds();
+        Assert.Equal(14*5, ids.Count());
+    }
+
+    [Fact]
+    public void SingleStepSequenceElementsIdsTest() {
+        var ids = ElementsIdTable.SingleStepSequenceElementsIds();
+        Assert.Equal(5+1, ids.Count());
+    }
+
+    [Fact]
+    public void PairJumpElementsIdsTest() {
+        var ids = ElementsIdTable.PairJumpElementsIds();
+        Assert.Equal(25, ids.Count());
+    }
+
+    [Fact]
+    public void PairStepSequenceElementsIdsTest() {
+        var ids = ElementsIdTable.PairStepSequenceElementsIds();
+        Assert.Equal(6, ids.Count());
+    }
+
+    [Fact]
+    public void PairLiftElementsIdsTest() {
+        var ids = ElementsIdTable.PairLiftElementsIds();
+        Assert.Equal(9*5, ids.Count());
+    }
+
+    [Fact]
+    public void PairTwistLiftElementsIdsTest() {
+        var ids = ElementsIdTable.PairTwistLiftElementsIds();
+        Assert.Equal(4*5, ids.Count());
+    }
+
+    [Fact]
+    public void PairThrowJumpElementsIdsTest() {
+        var ids = ElementsIdTable.PairThrowJumpElementsIds();
+        Assert.Equal(23, ids.Count());
+    }
+
+    [Fact]
+    public void PairDeathSpiralElementsIdsTest() {
+        var ids = ElementsIdTable.PairDeathSpiralElementsIds();
+        Assert.Equal(4*5+1, ids.Count());
+    }
+
+    [Fact]
+    public void PairSpinElementsIdsTest() {
+        var ids = ElementsIdTable.PairSpinElementsIds();
+        Assert.Equal(2*5, ids.Count());
+    }
+
+    [Fact]
+    public void IceDancePatternDanceElementsIdsTest() {
+        var ids = ElementsIdTable.IceDancePatternDanceElementsIds();
+        Assert.Equal(3*4, ids.Count());
+    }
+
+    [Fact]
+    public void IceDanceSpinElementsIdsTest() {
+        var ids = ElementsIdTable.IceDanceSpinElementsIds();
+        Assert.Equal(2*4, ids.Count());
+    }
+
+    [Fact]
+    public void IceDanceLiftElementsIdsTest() {
+        var ids = ElementsIdTable.IceDanceLiftElementsIds();
+        Assert.Equal(4*4, ids.Count());
+    }
+
+    [Fact]
+    public void IceDanceTwizzleElementsIdsTest() {
+        var ids = ElementsIdTable.IceDanceTwizzleElementsIds();
+        Assert.Equal(4, ids.Count());
+    }
+
+    [Fact]
+    public void IceDanceStepSequenceElementsIdsTest() {
+        var ids = ElementsIdTable.IceDanceStepSequenceElementsIds();
+        Assert.Equal(7*5, ids.Count());
+    }
+
+    [Fact]
+    public void IceDanceChoreographicElementsIdsTest() {
+        var ids = ElementsIdTable.IceDanceChoreographicElementsIds();
+        Assert.Equal(3, ids.Count());
+    }
+}
