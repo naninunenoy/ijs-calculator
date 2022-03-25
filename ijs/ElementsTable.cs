3906da1ff944aa6af9dac90854b688e321075da1
naninunenoy <naninuneno0411@gmail.com>
2022-03-25 20:08:20 +0900

    IDの元になった一覧をわかりやすく

--- ijs/ElementsTable.cs
+++ ijs/ElementsTable.cs
@@ -7,35 +7,42 @@ public static class ElementsTable {
     public static IEnumerable<Elements> SingleElementsList => SingleElementsDict.Values;
     public static IEnumerable<Elements> PairElementsList => PairElementsDict.Values;
     public static IEnumerable<Elements> IceDanceElementsList => IceDanceElementsDict.Values;
-
+    
     static ElementsTable() {
         SingleElementsDict = ElementsTypeList.Single()
-            .Select(x => (x, ElementsIdTable.SingleJumpElementsIds()
-                .Concat(ElementsIdTable.SingleSpinElementsIds())
-                .Concat(ElementsIdTable.SingleStepSequenceElementsIds())))
+            .Select(x => (x, ConcatIds(
+                ElementsIdTable.SingleJumpElementsIds(),
+                ElementsIdTable.SingleSpinElementsIds(),
+                ElementsIdTable.SingleStepSequenceElementsIds())))
             .SelectMany(ToElements)
             .ToDictionary(x => x.Id);
         PairElementsDict = ElementsTypeList.Pair()
-            .Select(x => (x, ElementsIdTable.PairJumpElementsIds()
-                .Concat(ElementsIdTable.PairStepSequenceElementsIds())
-                .Concat(ElementsIdTable.PairLiftElementsIds())
-                .Concat(ElementsIdTable.PairTwistLiftElementsIds())
-                .Concat(ElementsIdTable.PairThrowJumpElementsIds())
-                .Concat(ElementsIdTable.PairDeathSpiralElementsIds())
-                .Concat(ElementsIdTable.PairSpinElementsIds())))
+            .Select(x => (x, ConcatIds(
+                ElementsIdTable.PairJumpElementsIds(),
+                ElementsIdTable.PairStepSequenceElementsIds(),
+                ElementsIdTable.PairLiftElementsIds(),
+                ElementsIdTable.PairTwistLiftElementsIds(),
+                ElementsIdTable.PairThrowJumpElementsIds(),
+                ElementsIdTable.PairDeathSpiralElementsIds(),
+                ElementsIdTable.PairSpinElementsIds())))
             .SelectMany(ToElements)
             .ToDictionary(x => x.Id);
         IceDanceElementsDict = ElementsTypeList.IceDance()
-            .Select(x => (x, ElementsIdTable.IceDancePatternDanceElementsIds()
-                .Concat(ElementsIdTable.IceDanceSpinElementsIds())
-                .Concat(ElementsIdTable.IceDanceLiftElementsIds())
-                .Concat(ElementsIdTable.IceDanceTwizzleElementsIds())
-                .Concat(ElementsIdTable.IceDanceStepSequenceElementsIds())
-                .Concat(ElementsIdTable.IceDanceChoreographicElementsIds())))
+            .Select(x => (x, ConcatIds(
+                ElementsIdTable.IceDancePatternDanceElementsIds(),
+                ElementsIdTable.IceDanceSpinElementsIds(),
+                ElementsIdTable.IceDanceLiftElementsIds(),
+                ElementsIdTable.IceDanceTwizzleElementsIds(),
+                ElementsIdTable.IceDanceStepSequenceElementsIds(),
+                ElementsIdTable.IceDanceChoreographicElementsIds())))
             .SelectMany(ToElements)
             .ToDictionary(x => x.Id);
     }
-
+    
+    static IEnumerable<ElementsId> ConcatIds(params IEnumerable<ElementsId>[] ids) {
+        return ids.Aggregate(Enumerable.Empty<ElementsId>(), (total, x) => total.Concat(x));
+    }
+    
     static IEnumerable<Elements> ToElements((ElementsType elementType, IEnumerable<ElementsId> elementsIds) tuple) {
         var (type, ids) = tuple;
         return ids.Select(id => new Elements(
