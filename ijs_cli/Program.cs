1f1f97dde59df2b21cec82e9c951dd2e103a861b
naninunenoy <naninuneno0411@gmail.com>
2022-03-26 18:49:04 +0900

    Cliのパラメータパース

--- ijs_cli/Program.cs
+++ ijs_cli/Program.cs
@@ -1,7 +1,45 @@
-﻿// $ dotnet run -- 4S-4T+3T-CCSp4-3Ax-FSSp4-StSq3-CCoSp4
+﻿using System.CommandLine;
+
+// $ dotnet run -- --program 4S-4T+3T-CCSp4-3Ax-FSSp4-StSq3-CCoSp4
 // エレメントは `-` で区切る
 // 連続ジャンプは `+` で区切る
 // 後半ジャンプは末尾に `x` を付与する
-// -t single/pair/icedance で種目を判別(デフォルトはsingle)
+// --sports single/pair/icedance で種目を判別(デフォルトはsingle)
 
-Console.WriteLine("Hello, World!");
+const string programDescription =
+    "以下のようにプログラムを入力してください --program 4S-4T+3T-CCSp4-3Ax-FSSp4-StSq3-CCoSp4\n" +
+    "* エレメントは - で区切ってください\n" +
+    "* 連続ジャンプは + で区切ってください\n" +
+    "* 後半のジャンプには末尾に x をつけてください";
+
+var programOption = new Option<string>(
+    "--program",
+    getDefaultValue: () => "",
+    description: programDescription);
+
+var sportsOption = new Option<string>(
+    "--sports",
+    getDefaultValue: () => "single",
+    description: "single/pair/icedance を指定してください\n" +
+                 "何も入力していない場合はシングルになります");
+
+// Add the options to a root command:
+var rootCommand = new RootCommand
+{
+    programOption,
+    sportsOption
+};
+
+rootCommand.Description = "IJS Cli";
+
+rootCommand.SetHandler((string programArgs, string sportType) =>
+{
+    if (string.IsNullOrEmpty(programArgs)) {
+        Console.WriteLine(programDescription);
+        return;
+    }
+    // TODO
+}, programOption, sportsOption);
+
+// Parse the incoming args and invoke the handler
+return rootCommand.Invoke(args);
