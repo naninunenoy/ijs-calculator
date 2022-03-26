90830b97fc757846a722be6edd8b89c33693e146
naninunenoy <naninuneno0411@gmail.com>
2022-03-26 22:38:33 +0900

    個別の点数も表示

--- ijs_cli/ElementListExtractor.cs
+++ ijs_cli/ElementListExtractor.cs
@@ -1,4 +1,6 @@
-﻿using ijs;
+﻿using System.Globalization;
+using System.Text;
+using ijs;
 
 namespace ijs_cli;
 
@@ -9,5 +11,20 @@ public class ElementListExtractor {
         this.elementList = elementList;
     }
 
-    public string Extract() => "TODO"; //TODO
+    public string Extract() {
+        var stringBuilder = new StringBuilder();
+        var index = 1;
+        stringBuilder.AppendLine("=================== 内訳 ===================");
+        foreach (var x in elementList.List) {
+            var score = x.BaseValue.ToString("F2", CultureInfo.InvariantCulture);
+            var line = $"{index}. {x.Name}({x.FullCode}): {score}点";
+            if (x is SecondHalfElement) {
+                line += $" ※後半につき{SecondHalfElement.Magnification:F1}倍加点";
+            }
+            stringBuilder.AppendLine(line);
+            index++;
+        }
+        stringBuilder.AppendLine("===========================================");
+        return stringBuilder.ToString();
+    }
 }
