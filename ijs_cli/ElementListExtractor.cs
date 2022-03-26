ecdd797bda0b73e7c9a66a685bd3c4a57e47cddd
naninunenoy <naninuneno0411@gmail.com>
2022-03-26 23:24:51 +0900

    点数が縦にそろうよう善処

--- ijs_cli/ElementListExtractor.cs
+++ ijs_cli/ElementListExtractor.cs
@@ -1,6 +1,7 @@
 ﻿using System.Globalization;
 using System.Text;
 using ijs;
+using Microsoft.VisualBasic;
 
 namespace ijs_cli;
 
@@ -12,19 +13,42 @@ public class ElementListExtractor {
     }
 
     public string Extract() {
-        var stringBuilder = new StringBuilder();
-        var index = 1;
-        stringBuilder.AppendLine("=================== 内訳 ===================");
+        var lineNumber = 1;
+        var lines = new List<Line>();
         foreach (var x in elementList.List) {
-            var score = x.BaseValue.ToString("F2", CultureInfo.InvariantCulture);
-            var line = $"{index}. {x.Name}({x.FullCode}): {score}点";
+            var header = $"{lineNumber++}. {x.Name}({x.FullCode}):";
+            var content = x.BaseValue.ToString("F2", CultureInfo.InvariantCulture);
             if (x is SecondHalfElement) {
-                line += $" ※後半につき{SecondHalfElement.Magnification:F1}倍加点";
+                content += $" ※後半につき{SecondHalfElement.Magnification:F1}倍加点";
             }
-            stringBuilder.AppendLine(line);
-            index++;
+            lines.Add(new Line(header, content));
+        }
+
+        var maxHeaderLength = lines.Max(x => x.GetHeaderLength());
+        var stringBuilder = new StringBuilder();
+        stringBuilder.AppendLine("=================== 内訳 ===================");
+        foreach (var line in lines) {
+            var spaceNum = maxHeaderLength - line.GetHeaderLength();
+            stringBuilder.AppendLine(line.ToString(spaceNum));
         }
         stringBuilder.AppendLine("===========================================");
         return stringBuilder.ToString();
+    }
+
+    readonly struct Line {
+        const int TabLength = 8;
+        public readonly string Header;
+        public readonly string Content;
+        public Line(string header, string content) {
+            Header = header;
+            Content = content;
+        }
+        public int GetHeaderLength() {
+            return Encoding.Unicode.GetBytes(Header).Length;
+        }
+        public string ToString(int spaceNum) {
+            var tabNum = spaceNum / TabLength;
+            return $"{Header}{new string('\t', tabNum < 1 ? 1 : tabNum)}{Content}";
+        }
     }
 }
