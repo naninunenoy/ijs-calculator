e73a6112924c06b0cdda30688480968cd269f468
naninunenoy <naninuneno0411@gmail.com>
2022-03-26 18:12:34 +0900

    後半ジャンプの加点

---
+++ ijs/SecondHalfElement.cs
@@ -0,0 +1,19 @@
+﻿namespace ijs;
+
+public class SecondHalfElement : IElement {
+    public const float MultipleValue = 1.1F;
+    readonly IElement element;
+
+    public SecondHalfElement(IElement element) {
+        this.element = element;
+    }
+
+    public bool Equals(IElement? other) {
+        return element.Equals(other);
+    }
+
+    public string Name => element.Name;
+    public string FullCode => element.FullCode;
+    public ElementType ElementType => element.ElementType;
+    public float BaseValue => element.BaseValue * MultipleValue;
+}
---
+++ ijs_test/SecondHalfElementTest.cs
@@ -0,0 +1,14 @@
+﻿using ijs;
+using ijs_test.TestDriver;
+using Xunit;
+
+namespace ijs_test; 
+
+public class SecondHalfElementTest {
+    [Fact]
+    public void MultipleTest() {
+        var jump = new TestElement("ジャンプ", "Jump", ElementType.Jump, 10F);
+        var secondHalf = new SecondHalfElement(jump);
+        Assert.Equal(11F, secondHalf.BaseValue);
+    }
+}
