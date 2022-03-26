8fa72ea6501615c3e581ab977d0228ee48e41704
naninunenoy <naninuneno0411@gmail.com>
2022-03-26 18:13:40 +0900

    fix field name

--- ijs/SecondHalfElement.cs
+++ ijs/SecondHalfElement.cs
@@ -1,7 +1,7 @@
 ﻿namespace ijs;
 
 public class SecondHalfElement : IElement {
-    public const float MultipleValue = 1.1F;
+    public const float Magnification = 1.1F;
     readonly IElement element;
 
     public SecondHalfElement(IElement element) {
@@ -15,5 +15,5 @@ public class SecondHalfElement : IElement {
     public string Name => element.Name;
     public string FullCode => element.FullCode;
     public ElementType ElementType => element.ElementType;
-    public float BaseValue => element.BaseValue * MultipleValue;
+    public float BaseValue => element.BaseValue * Magnification;
 }
