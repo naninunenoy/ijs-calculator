8b9870c6281ce3ada928e5a8a3db2f6aa45c5b90
naninunenoy <naninuneno0411@gmail.com>
2022-03-26 18:23:16 +0900

    cliの仕様

---
+++ ijs_cli/Program.cs
@@ -0,0 +1,7 @@
+﻿// $ dotnet run -- 4S-4T+3T-CCSp4-3Ax-FSSp4-StSq3-CCoSp4
+// エレメントは `-` で区切る
+// 連続ジャンプは `+` で区切る
+// 後半ジャンプは末尾に `x` を付与する
+// -t single/pair/icedance で種目を判別(デフォルトはsingle)
+
+Console.WriteLine("Hello, World!");
