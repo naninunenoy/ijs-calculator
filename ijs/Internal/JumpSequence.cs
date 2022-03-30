821c1b6cb39c56fe32adc63d4969956b1fd32c93
naninunenoy <naninuneno0411@gmail.com>
2022-03-30 11:50:18 +0900

    連続ジャンプの判定修正

--- ijs/Internal/JumpSequence.cs
+++ ijs/Internal/JumpSequence.cs
@@ -9,23 +9,8 @@ public class JumpSequence : IElement {
         jumps = new CombinationJump();
     }
 
-    public void Build(params IElement[] jumpElements) {
-        if (jumpElements.Length < 1) {
-            throw new ArgumentException("ジャンプの数が足りていません");
-        }
-        if (jumpElements.Any(x => x.ElementType is not ElementType.Jump)) {
-            throw new ArgumentException("ジャンプでないエレメンツが含まれています");
-        }
-        if (jumpElements.Length > maxJumpCount) {
-            throw new ArgumentException($"ジャンプシークエンスは最高{maxJumpCount}回です");
-        }
-        jumps.Build(jumpElements);
-    }
-
-    public bool Equals(IElement? other) {
-        return jumps.Equals(other);
-    }
-
+    public void Build(params IElement[] jumpElements) => jumps.Build(jumpElements);
+    public bool Equals(IElement? other) => jumps.Equals(other);
     public string Name => jumps.Name;
     public string FullCode => $"{jumps.FullCode}+SEQ";
     public ElementType ElementType => jumps.ElementType;
